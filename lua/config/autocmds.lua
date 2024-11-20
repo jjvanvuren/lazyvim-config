34587c96f2642c60f4f23fa2058accfca841e48d
Jacques van Vuren <jacques.vanvuren@locks.com.au>
2024-11-20 14:14:25 +1100

    File type based settings

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "python",
+    callback = function()
+        -- Set rulers at 80 and 120 characters
+        vim.opt_local.colorcolumn = "80,120"
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "python", "javascript" },
+    callback = function()
+        -- Number of spaces to use for autoindenting
+        vim.opt_local.shiftwidth = 4
+
+        -- Disable LazyVim auto format
+        vim.b.autoformat = false
+    end,
+})
