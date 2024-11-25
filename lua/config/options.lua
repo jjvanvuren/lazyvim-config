6728ec0bda91c7aab117acf3976f310313510a1c
Jacques van Vuren <jacques.vanvuren@locks.com.au>
2024-11-25 16:20:37 +1100

    Show all spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.list = true
+vim.opt.listchars = {
+  space = "·", -- Show spaces as dots
+  tab = "→ ", -- Show tabs with an arrow
+  trail = "⋅", -- Show trailing spaces
+  extends = "⟩", -- Show lines that extend beyond the screen
+  precedes = "⟨", -- Show lines that wrap to the left
+}
