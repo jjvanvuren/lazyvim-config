74c513a3ae9f31fafde08174c71a1545c4897434
jjvanvuren <jacquesvv@duck.com>
2024-12-03 10:21:20 +1100

    Remove tokyonight & use copilot.nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,18 +3,13 @@
 -- Add any additional autocmds here
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "python",
-  callback = function()
-    -- Set rulers at 80 and 120 characters
-    vim.opt_local.colorcolumn = "80,120"
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
   pattern = { "python", "javascript" },
   callback = function()
     -- Number of spaces to use for autoindenting
     vim.opt_local.shiftwidth = 4
+
+    -- Set rulers at 80 and 120 characters
+    vim.opt_local.colorcolumn = "80,120"
 
     -- Disable LazyVim auto format
     vim.b.autoformat = false
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,25 @@
 return {
   {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+  {
     "catppuccin/nvim",
     as = "catppuccin",
     lazy = false,
-    priority= 1000,
+    priority = 1000,
     config = function()
       require("catppuccin").setup({
         flavour = "macchiato", -- Choose between "latte", "frappe", "macchiato", "mocha"
+        transparent_background = true,
       })
       vim.cmd("colorscheme catppuccin")
-    end
-  }
+    end,
+  },
 }
