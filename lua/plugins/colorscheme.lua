39f30f51001e7f3af25fadd6b0cdb1eeb1a310dc
jjvanvuren <jacquesvv@duck.com>
2025-10-09 11:24:09 +1100

    Add rovo dev plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,12 +11,16 @@ return {
   },
   {
     "catppuccin/nvim",
-    lazy = false,
     name = "catppuccin",
+    lazy = false,
     priority = 1000,
     opts = {
-      flavour = "macchiato", -- Choose between "latte", "frappe", "macchiato", "mocha"
-      transparent_background = true,
+      flavour = "macchiato", -- latte, frappe, macchiato, mocha
+      transparent_background = vim.g.transparent_background,
+      float = {
+        transparent = vim.g.transparent_background, -- enable transparent floating windows
+        solid = false, -- use solid styling for floating windows, see |winborder|
+      },
     },
   },
 }
